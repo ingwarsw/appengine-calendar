b4c592a4c38885256e324f7d3793335969c04353
Karol Lassak <karol.lassak@coi.gov.pl>
2014-09-07 17:06:01 +0200

    Added looking for holidays
    - Slow motion but should work

--- src/main/java/org/eu/ingwar/appengine/calendar/utils/ServiceUtils.java
+++ src/main/java/org/eu/ingwar/appengine/calendar/utils/ServiceUtils.java
@@ -7,24 +7,51 @@ import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.CalendarScopes;
+import com.google.api.services.calendar.model.Event;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import org.apache.commons.lang3.time.DateUtils;
 
 public class ServiceUtils {
-  
-//  /** Global instance of the HTTP transport. */
-  static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
 
-  /** Global instance of the JSON factory. */
-  static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-  
-  public static Calendar getCalendarInstance() throws IOException {
-    AppIdentityCredential credential = new AppIdentityCredential(Arrays.asList(CalendarScopes.CALENDAR));
-    Calendar service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential).setApplicationName("whateva").build();
-    return service;
-  }
+    public static final String HOLIDAYS_ID = "en.polish#holiday@group.v.calendar.google.com";
 
-  private ServiceUtils() {
-  }
+    /**
+     * Global instance of the HTTP transport.
+     */
+    private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+
+    /**
+     * Global instance of the JSON factory.
+     */
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+
+    private static Calendar instance;
+
+    public static Calendar getCalendarInstance() throws IOException {
+        if (instance == null) {
+            AppIdentityCredential credential = new AppIdentityCredential(Arrays.asList(CalendarScopes.CALENDAR));
+            instance = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential).setApplicationName("whateva").build();
+        }
+        return instance;
+    }
+    
+    
+    public static boolean isHoliday(Date date) throws IOException {
+        List<Event> holidays = getCalendarInstance().events().list(HOLIDAYS_ID).execute().getItems();
+        
+        for (Event ev : holidays) {
+            Date eventDate = new Date(ev.getStart().getDate().getValue());
+            if (DateUtils.isSameDay(date, eventDate)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private ServiceUtils() {
+    }
 
 }
